4e640e2767e7e858d9e4b0798c2f08f9d13fe0cf
Khanh <khanhmeizu1@gmail.com>
2023-08-03 17:02:44 +0700

    third commit

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import PageComponent from './components/PageComponent';
+import { userStateContext } from '../contexts/ContextProvider';
+import SurveyListItem from './components/SurveyListItem';
 
 const Surveys = () => {
+
+    const {surveys} = userStateContext();
+    console.log(surveys)
+
+    const onDeleteClick = () => {
+        console.log('onclick delete click')
+    }
+
     return (
         <PageComponent title="Surveys">
-            Surveys content
+            <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3'>
+                {
+                    surveys.map((survey) => 
+                    <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick} />)
+                }
+            </div>
         </PageComponent>
     );
 };
